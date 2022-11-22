f4951468729cbd8f2fd7264b4bc5ecf8297e0212
Chris Dakin <christopher.dakin@shopify.com>
2022-11-21 21:58:58 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const documentHeight = () => {
+  const doc = document.documentElement;
+  doc.style.setProperty("--doc-height", `${window.innerHeight - 100}px`);
+  doc.style.setProperty("--doc-width", `${window.innerWidth - 25}px`);
+};
+window.addEventListener("resize", documentHeight);
+documentHeight();
+
+const button = document.getElementById("button");
+const reset = document.getElementById("reset");
+
+const timer = document.getElementById("timer");
+timer.innerHTML = "5.00";
+
+let time = 500;
+let clicked = 0;
+let interval;
+
+button.addEventListener("click", () => {
+  if (time === 500) {
+    button.innerHTML = 1;
+    clicked = 1;
+    interval = setInterval(() => {
+      if (time <= 0) {
+        button.innerHTML = "Total: " + clicked;
+        timer.innerHTML = "0.00";
+        clearInterval(interval);
+      } else {
+        time = time - 1;
+        let updatedTimer = time.toString();
+        updatedTimer = (time / 100).toFixed(2);
+        timer.innerHTML = updatedTimer;
+        console.log(time);
+      }
+    }, 10);
+  } else if (time > 0) {
+    clicked++;
+    button.innerHTML = clicked;
+  }
+});
+
+reset.addEventListener("click", () => {
+  clearInterval(interval);
+  time = 500;
+  clicked = 0;
+  button.innerHTML = "Start";
+  timer.innerHTML = "5.00";
+});
