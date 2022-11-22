8c6922705bbb886d1e914a65264c9f490b588eaa
Chris Dakin <christopher.dakin@shopify.com>
2022-11-21 22:20:59 -0600

    Try to prevent zoom

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ const documentHeight = () => {
 window.addEventListener("resize", documentHeight);
 documentHeight();
 
+document.addEventListener("gesturestart", function (e) {
+  e.preventDefault();
+  // special hack to prevent zoom-to-tabs gesture in safari
+  document.body.style.zoom = 0.99;
+});
+
+document.addEventListener("gesturechange", function (e) {
+  e.preventDefault();
+  // special hack to prevent zoom-to-tabs gesture in safari
+  document.body.style.zoom = 0.99;
+});
+
+document.addEventListener("gestureend", function (e) {
+  e.preventDefault();
+  // special hack to prevent zoom-to-tabs gesture in safari
+  document.body.style.zoom = 0.99;
+});
+
 const button = document.getElementById("button");
 const reset = document.getElementById("reset");
 
